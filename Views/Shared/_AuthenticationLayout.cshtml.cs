dc27e3ee4cc144f6b24b8595f982cb3548798f9c
Marcelly Freitas <marcellymoraes2014@gmail.com>
2026-06-28 18:55:28 -0300

    feat: adicionar layouts, estilos e assets front-end

---
+++ Views/Shared/_AuthenticationLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OfficeRoomie.Views.Shared
+{
+    public class _AuthenticationLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Views/Shared/_HomeLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OfficeRoomie.Views.Shared
+{
+    public class _HomeLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
