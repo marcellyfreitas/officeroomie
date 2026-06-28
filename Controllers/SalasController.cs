0abb09336e8cff757c3e4b45c4d400acf555936b
Marcelly Freitas <marcellymoraes2014@gmail.com>
2026-06-28 18:55:28 -0300

    feat: implementar gerenciamento de salas

---
+++ Controllers/SalasController.cs
@@ -0,0 +1,167 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OfficeRoomie.Database;
+using OfficeRoomie.Models;
+using OfficeRoomie.Models.ViewModels;
+
+namespace OfficeRoomie.Controllers
+{
+    public class SalasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SalasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Salas
+        public async Task<IActionResult> Index(int? pageNumber, string searchString, string currentFilter)
+        {
+            if (searchString != null) {
+                pageNumber = 1;
+            } else {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            var salas = _context.Salas.AsNoTracking().OrderByDescending(a => a.id);
+            var salasFiltradas = !String.IsNullOrEmpty(searchString)
+                ? salas.Where(s => s.nome.ToLower().Contains(searchString.ToLower()))
+                : salas;
+            var salasPaginadas = await ModelPaginado<Sala>.CreateAsync(salasFiltradas, pageNumber ?? 1, 5);
+
+            return View(salasPaginadas);
+        }
+
+        // GET: Salas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sala = await _context.Salas
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (sala == null)
+            {
+                return NotFound();
+            }
+
+            return View(sala);
+        }
+
+        // GET: Salas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Salas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,nome,descricao,capacidade,categoria,created_at,updated_at")] Sala sala)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sala);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sala);
+        }
+
+        // GET: Salas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sala = await _context.Salas.FindAsync(id);
+            if (sala == null)
+            {
+                return NotFound();
+            }
+            return View(sala);
+        }
+
+        // POST: Salas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,nome,descricao,capacidade,categoria,created_at,updated_at")] Sala sala)
+        {
+            if (id != sala.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sala);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SalaExists(sala.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sala);
+        }
+
+        // GET: Salas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sala = await _context.Salas
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (sala == null)
+            {
+                return NotFound();
+            }
+
+            return View(sala);
+        }
+
+        // POST: Salas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sala = await _context.Salas.FindAsync(id);
+            if (sala != null)
+            {
+                _context.Salas.Remove(sala);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SalaExists(int id)
+        {
+            return _context.Salas.Any(e => e.id == id);
+        }
+    }
+}
